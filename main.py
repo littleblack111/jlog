ae02693350eba8b3b1a82ed0f3692b4e41a43f76
littleblack111 <littleblack11111@gmail.com>
2023-10-17 14:01:47 +0800

    added menu & added argument that cna pass through to write.py or read.py

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
+#!/usr/bin/env python
+
 from sys import argv, executable
 from os import execv
-if argv[1] == 'write':
-    execv(executable, ['python3', 'write.py'])
-elif argv[1] =='read':
-    execv(executable, ['python3','read.py'])
+if len(argv) > 1:
+    if argv[1] == 'write':
+        print(' '.join(argv[2:]))
+        execv(executable, ['python3', 'write.py'] + argv[2:])
+    elif argv[1] =='read':
+        execv(executable, ['python3','read.py'])
+    else:
+        raise Exception("Unknown command\n\tAvailable commands: write, read")
 else:
-    raise Exception("Unknown command")
+    from simple_term_menu import TerminalMenu
+    choice = TerminalMenu(['Write', 'Read']).show()
+    if choice == 0:
+        execv(executable, ['python3', 'write.py'])
+    elif choice == 1:
+        execv(executable, ['python3','read.py'])
